ebcd2ad4290453b0d234ff6601d718da94af5e36
llaurentiu <laurentiu.latu@gmail.com>
2016-04-27 17:43:09 +0200

    fix griddle.jsx.js

--- modules/griddle.jsx.js
+++ modules/griddle.jsx.js
@@ -140,7 +140,7 @@ var Griddle = React.createClass({
         uniqueIdentifier: React.PropTypes.string
     },
     defaultFilter: function defaultFilter(results, filter) {
-        var colMetadata = (that.columnSettings.columnMetadata || []).reduce(function (previous, current) {
+        var colMetadata = (this.columnSettings.columnMetadata || []).reduce(function (previous, current) {
             previous[current.columnName] = current;
             return previous;
         }, {});
