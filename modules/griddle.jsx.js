9ef47e27d7627384ccaece8c3c2e2f35ba96f9f6
llaurentiu <laurentiu.latu@gmail.com>
2016-08-16 11:23:49 +0200

    Update griddle.jsx.js
    formatting

--- modules/griddle.jsx.js
+++ modules/griddle.jsx.js
@@ -368,10 +368,10 @@ var Griddle = React.createClass({
             this.columnSettings.allColumns = [];
         }
         /*Prevent reseting the filtered columns (in grid settings) to the initial ones when setting the state in the parent component of the griddle*/
-	     if(this.prevColumns.length !== (nextProps.columns || []).length || !_.isEqual(this.prevColumns.sort(),(nextProps.columns || []).sort()))
+	if(this.prevColumns.length !== (nextProps.columns || []).length || !_.isEqual(this.prevColumns.sort(),(nextProps.columns || []).sort()))
         {
           this.columnSettings.filteredColumns = (nextProps.columns || []).slice();
-		    this.prevColumns = (nextProps.columns || []).slice();
+          this.prevColumns = (nextProps.columns || []).slice();
         }
 
         if (nextProps.selectedRowIds) {
