38dfe1a57468a5ed9cee3418ab03e73a716e18f8
llaurentiu <laurentiu.latu@gmail.com>
2017-06-21 18:08:16 +0200

    Update gridTable.jsx.js

--- modules/gridTable.jsx.js
+++ modules/gridTable.jsx.js
@@ -281,7 +281,8 @@ var GridTable = (function (_React$Component) {
                 });
 
                 if (this.props.showNoData) {
-                    var colSpan = this.props.columnSettings.getVisibleColumnCount();
+                    //fix IE bug - it throws exception for colSpan 0
+                    var colSpan = this.props.columnSettings.getVisibleColumnCount() || 1;
                     nodes.push(_react2['default'].createElement('tr', { key: 'no-data-section' }, _react2['default'].createElement('td', { colSpan: colSpan }, this.props.noDataSection)));
                 }
 
